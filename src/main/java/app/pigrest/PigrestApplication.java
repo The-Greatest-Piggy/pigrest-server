193169f63f1eddc69eae8351a6f31acbebb87add
kanchoeun <kancho1216@naver.com>
2024-10-28 18:27:06 +0900

    chore: initial settings

---
+++ src/main/java/app/pigrest/PigrestApplication.java
@@ -0,0 +1,13 @@
+package app.pigrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigrestApplication.class, args);
+	}
+
+}
