f5d958710578753ef176eb391e9a649bec1c1680
KANCHOEUN <kancho1216@naver.com>
2025-04-07 14:52:44 +0900

    feat: add Redis configuration and service (#1)

---
+++ src/main/java/app/pigrest/auth/service/RedisService.java
@@ -0,0 +1,25 @@
+package app.pigrest.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisService {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public void setValue(String key, String value, long timeout, TimeUnit unit) {
+        redisTemplate.opsForValue().set(key, value, timeout, unit);
+    }
+
+    public String getValue(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    public void deleteValue(String key) {
+        redisTemplate.delete(key);
+    }
+}
