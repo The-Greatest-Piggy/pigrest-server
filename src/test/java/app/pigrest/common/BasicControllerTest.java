8cb82167dfd23c8ecc48124cfe5a20638732609c
KANCHOEUN <kancho1216@naver.com>
2025-04-15 15:43:37 +0900

    test: add BasicControllerTest for setup (#24)

---
+++ src/test/java/app/pigrest/common/BasicControllerTest.java
@@ -0,0 +1,37 @@
+package app.pigrest.common;
+
+import app.pigrest.auth.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+
+@AutoConfigureRestDocs
+@ExtendWith(RestDocumentationExtension.class)
+public class BasicControllerTest {
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @MockBean
+    protected JwtService jwtService;
+
+    @BeforeEach
+    public void setup(WebApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation).operationPreprocessors()
+                        .withResponseDefaults())
+                .build();
+    }
+}
